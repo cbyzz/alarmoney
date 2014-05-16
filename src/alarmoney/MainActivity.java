b34e8f130c87adbb5789d41e0dc383d4b40b39b9
조국현 <cho.kukhyun@cashslide.co.kr>
2014-05-16 16:52:56 +0900

    tabbar added

--- src/alarmoney/MainActivity.java
+++ src/alarmoney/MainActivity.java
@@ -1,13 +1,85 @@
 package alarmoney;
 
 import insomnia.alarmoney.R;
-import android.app.Activity;
-import android.os.Bundle;
 
-public class MainActivity extends Activity {
-	 protected void onCreate(Bundle savedInstanceState) {
-        setContentView(R.layout.activity_main);
-        
-        super.onCreate(savedInstanceState);    
+import java.util.Locale;
+
+import alarmoney.views.TabBarView;
+import alarmoney.views.TabBarView.OnTabListener;
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+
+public class MainActivity extends FragmentActivity implements OnTabListener {
+	
+	private static final String LOG_TAG = MainActivity.class.getSimpleName();
+	
+	private SectionsPagerAdapter mSectionsPagerAdapter = null;
+	private TabBarView mTabBar = null;
+	private ViewPager mViewPager = null;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		Log.i(LOG_TAG, "onCreate");
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_main);
+	
+		final ActionBar actionBar = getActionBar();
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+		
+		mTabBar = (TabBarView) findViewById(R.id.tabbar);
+		
+		mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+		mViewPager = (ViewPager) findViewById(R.id.pager);
+		mViewPager.setAdapter(mSectionsPagerAdapter);
+
+		mViewPager.setOnPageChangeListener(mTabBar);
 	}
+	
+	@Override
+	public void onTabSelected(int position) {
+		Log.i(LOG_TAG, "onTabSelected");
+		mViewPager.setCurrentItem(position);
+	}
+
+	public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+		public SectionsPagerAdapter(FragmentManager fm) {
+			super(fm);
+		}
+
+		@Override
+		public Fragment getItem(int position) {
+			return new Fragment();
+		}
+
+		@Override
+		public int getCount() {
+			return 4;
+		}
+
+		@Override
+		public CharSequence getPageTitle(int position) {
+			Locale l = Locale.getDefault();
+			switch (position) {
+			case 0:
+				return "0";
+			case 1:
+				return "1";
+			case 2:
+				return "2";
+			case 3:
+				return "3";
+			}
+			return null;
+		}
+	}
+
 }
---
+++ src/alarmoney/views/TabBarView.java
@@ -0,0 +1,215 @@
+package alarmoney.views;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.Display;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+public class TabBarView extends View implements OnPageChangeListener, OnTouchListener {
+	
+	private static final String LOG_TAG = TabBarView.class.getSimpleName();
+	
+	public interface OnTabListener {
+		public void onTabSelected(int position);
+	}
+	
+	private static final int SELECTED_INDICATOR_HEIGHT = 20;
+	private static final int UNSELECTED_INDICATOR_HEIGHT = 10;
+	
+	private ArrayList<View> _childList = new ArrayList<View>();
+	
+	private HashMap<View, Bitmap> _bitmapCache = new HashMap<View, Bitmap>();
+	
+	private int _selected = 0;
+	
+	private float _slideX = 0;
+	private Paint _indicatorPaint = null;
+	private int _indicatorColor = 0xffff0000;
+	
+	private OnTabListener _listener = null;
+	
+	public TabBarView(Context context) {
+		super(context);
+		initView();
+	}
+	
+	public TabBarView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		initView();
+	}
+
+	public TabBarView(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+		initView();
+	}
+	
+	private void initView() {
+		_indicatorPaint = new Paint();
+		_indicatorPaint.setColor(_indicatorColor);
+		addTabItemImage(android.R.drawable.btn_plus);
+		addTabItemImage(android.R.drawable.btn_minus);
+		addTabItemImage(android.R.drawable.btn_dialog);
+		addTabItemImage(android.R.drawable.btn_radio);
+		
+		setWillNotDraw(false);
+		
+		this.setOnTouchListener(this);
+	}
+	
+	public int getChildCount() {
+		return _childList.size();
+	}
+	
+	public View getChildAt(int position) {
+		return _childList.get(position);
+	}
+	
+	public void addView(View v) {
+		_childList.add(v);
+	}
+	
+	public void setOnTabListener(OnTabListener listener) {
+		_listener = listener;
+	}
+	
+	public int getPosition(View v) {
+		for (int i = 0; i < getChildCount(); i++) {
+			if (getChildAt(i) == v) {
+				return i;
+			}
+		}
+		
+		return -1;
+	}
+	
+	@Override
+	protected void onLayout(boolean changed, int l, int t, int r, int b) {
+	     int itemWidth = (r-l)/getChildCount();
+	     for(int i=0; i< this.getChildCount(); i++){
+	         View v = getChildAt(i);
+	         v.layout(itemWidth*i, t, (i+1)*itemWidth, b);
+	     }
+	}
+	
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec){
+		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+		
+		int height = 0;
+		for(int i=0; i<getChildCount(); i++){
+			View v = getChildAt(i);
+			v.measure(MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED), MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+			height = v.getMeasuredHeight();
+		}
+		
+		Log.d("proxima", String.format("measured height = %d", height));
+		
+		WindowManager wm = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
+		Display display = wm.getDefaultDisplay();
+		
+		height = (int)(((float) display.getHeight()) * 0.07);
+		
+		setMeasuredDimension(getMeasuredWidth(), height);
+	}
+	
+	public void addTabItemView(View v) {
+		addView(v);
+	}
+	
+	public void addTabItemImage(int res) {
+		ImageView imageView = new ImageView(getContext());
+		imageView.setBackgroundResource(res);
+		addTabItemView(imageView);
+	}
+	
+	public Bitmap loadBitmapFromView(View v) {
+		if (_bitmapCache.containsKey(v)) {
+			return _bitmapCache.get(v);
+		}
+		
+	    Bitmap b = Bitmap.createBitmap( v.getWidth(), v.getHeight(), Bitmap.Config.ARGB_8888);                
+	    Canvas c = new Canvas(b);
+	    v.layout(v.getLeft(), v.getTop(), v.getRight(), v.getBottom());
+	    v.draw(c);
+	    
+	    _bitmapCache.put(v, b);
+	    
+	    return b;
+	}
+
+	@Override
+	public void onDraw(Canvas canvas) {
+		
+		int width = canvas.getWidth();
+		int height = canvas.getHeight();
+		int indicatorWidth = getChildAt(0).getWidth();
+		
+		for (int i = 0; i < getChildCount(); i++) {
+			View v = getChildAt(i);
+			Bitmap bitmap = loadBitmapFromView(v);
+			canvas.drawBitmap(bitmap, i * indicatorWidth, 0, null);
+		}
+		
+		Log.d("proxima", String.format("slidex = %f, indicatorWidth = %d, width = %d, height = %d", _slideX, indicatorWidth, width, height));
+	
+		if (getChildCount() <= 0) {
+			return;
+		}
+		
+		canvas.drawRect(0, height - UNSELECTED_INDICATOR_HEIGHT, width, height, _indicatorPaint);
+		canvas.drawRect(_slideX , height - SELECTED_INDICATOR_HEIGHT, _slideX + indicatorWidth, height, _indicatorPaint);
+	}
+
+	@Override
+	public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+		if (getChildCount() <= 0) {
+			return;
+		}
+		
+		int indicatorWidth = getChildAt(0).getWidth();
+		
+		_slideX = indicatorWidth * (position + positionOffset);
+		invalidate();
+	
+		if (positionOffset == 0.0f || positionOffset == 1.0f) {
+			this._listener.onTabSelected(position);
+		}
+	}
+
+	@Override
+	public void onPageSelected(int position) {
+		_selected = position;
+	}
+
+	@Override
+	public void onPageScrollStateChanged(int arg0) {
+		
+	}
+
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		
+		int indicatorWidth = getChildAt(0).getWidth();
+		
+		int position = (int)(event.getX() / indicatorWidth);
+		
+		if (_listener != null) {
+			_listener.onTabSelected(position);
+		}
+		
+		return true;
+	}
+
+}
