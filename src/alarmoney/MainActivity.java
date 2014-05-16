bad42cbc68f973138e87509aa5d063ad7dcd271c
Choi byoung youl <choi.byoungyoul@cashslide.co.kr>
2014-05-16 15:12:50 +0900

    엑티비티 추가

---
+++ src/alarmoney/MainActivity.java
@@ -0,0 +1,13 @@
+package alarmoney;
+
+import insomnia.alarmoney.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+	 protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_main);
+        
+        super.onCreate(savedInstanceState);    
+	}
+}
