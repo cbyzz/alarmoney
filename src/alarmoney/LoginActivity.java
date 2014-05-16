3acacee532b244452d36a8087ddd878da62cbb06
Choi byoung youl <choi.byoungyoul@cashslide.co.kr>
2014-05-16 18:30:34 +0900

    첫화면

---
+++ src/alarmoney/LoginActivity.java
@@ -0,0 +1,14 @@
+package alarmoney;
+
+import insomnia.alarmoney.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginActivity extends Activity{
+	protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_login);
+        super.onCreate(savedInstanceState);  
+        
+        
+	}
+}
